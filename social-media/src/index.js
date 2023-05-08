27219e13397d22ba7d1ec4e28258e311b1dfcc45
Thijs Heerze <thijsheerze@gmail.com>
2023-05-08 11:36:58 +0200

    test

--- social-media/src/index.js
+++ social-media/src/index.js
@@ -11,7 +11,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
