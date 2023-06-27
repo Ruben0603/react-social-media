ae4e0deb6720af0b279d38ae848a078cf6768025
Thijs Heerze <thijsheerze@gmail.com>
2023-06-27 16:49:22 +0200

    last commit v2

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router';
 import './style/App.css';
 import Overview from './Overview';
 import Register from './Register';
-import Login from './Login';
 
 function App() {
   const [user] = useState();
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Overview/>} />
         <Route path="/:userId" element={<Overview credentials={user}/>} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
